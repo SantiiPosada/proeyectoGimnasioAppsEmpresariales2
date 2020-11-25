c2862865c01f134fcfdcb6333b05a461093a2ccf
SantiiPosada <50113058+SantiiPosada@users.noreply.github.com>
2020-11-25 15:16:09 -0500

    Se cuadra mensaje properties

--- gimnasio-1/src/main/java/co/com/eam/appsEmpresariales/domain/Socio.java
+++ gimnasio-1/src/main/java/co/com/eam/appsEmpresariales/domain/Socio.java
@@ -18,6 +18,7 @@ import java.util.List;
 @Entity
 
 
+
 //@NamedQuery(name = "Socio.findAll", query = "SELECT s FROM Socio s")
 public class Socio implements Serializable {
 	private static final long serialVersionUID = 1L;
